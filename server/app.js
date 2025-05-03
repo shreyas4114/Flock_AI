132849409258ec4c837499dc7d507d41cea528a2
shreyas4114 <shreyas04.fatale@gmail.com>
2025-05-04 00:39:20 +0530

    Minor backend changes

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const app = express();
-
+ 
 dotenv.config();
 app.use(cors());
 app.use(express.json());
